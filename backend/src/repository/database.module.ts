164b89c649749961d86ba2864eae284e1c3ed087
Irina <iyurasowa@yandex.ru>
2026-01-18 17:56:06 +0700

    TypeORM Module 3

--- backend/src/repository/database.module.ts
+++ backend/src/repository/database.module.ts
@@ -12,12 +12,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: configService.get<string>('DATABASE_DRIVER', 'postgres') as 'postgres',
-        url: configService.get<string>('DATABASE_DRIVER', 'postgres://localhost:5432/films'),
-        host: 'localhost',
-        port: 5432,
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: 'prac',
+        url: configService.get<string>('DATABASE_URL', 'postgres://prac:prac@localhost:5432/prac'),
         entities: [Film, Schedule],
         synchronize: false,
       }),
