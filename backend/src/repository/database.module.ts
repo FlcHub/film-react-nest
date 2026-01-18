a0d164d4b9fb3ff0e51d411876ed3d8469fa5e2a
Irina <iyurasowa@yandex.ru>
2026-01-18 18:35:35 +0700

    TypeORM Module 7

--- backend/src/repository/database.module.ts
+++ backend/src/repository/database.module.ts
@@ -8,17 +8,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forRoot()],
-      inject: [ConfigService],
+      imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: configService.get<string>('DATABASE_DRIVER', 'postgres') as 'postgres',
         host: 'localhost',
         port: 5432,
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
+        username: configService.get<string>('DATABASE_USERNAME', 'postgres'),
+        password: configService.get<string>('DATABASE_PASSWORD', 'postgres'),
+        database: 'films',
         entities: [Film, Schedule],
         synchronize: false,
       }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Film, Schedule]),
   ],
