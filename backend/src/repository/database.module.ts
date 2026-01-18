c07c4cec8adab3276ce397e5fe62184dcfcba8a6
Irina <iyurasowa@yandex.ru>
2026-01-18 17:36:01 +0700

    Замена тестов, немного изменен env

--- backend/src/repository/database.module.ts
+++ backend/src/repository/database.module.ts
@@ -12,11 +12,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: configService.get<string>('DATABASE_DRIVER', 'postgres') as 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
+        host: 'localhost',
         port: configService.get<number>('DATABASE_PORT', 5432),
         username: configService.get<string>('DATABASE_USERNAME'),
         password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        database: 'prac',
         entities: [Film, Schedule],
         synchronize: false,
       }),
