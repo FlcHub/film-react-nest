3f621e8a2e28ff439ae8004a61fbed6a9a890aa7
Irina <iyurasowa@yandex.ru>
2026-02-06 20:30:14 +0700

    Добавлены тесты для логгеров

--- backend/src/logger/json.logger.spec.ts
+++ backend/src/logger/json.logger.spec.ts
@@ -1,13 +1,94 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JsonLogger } from './json.logger';
 
-describe('UsersController', () => {
+describe('JsonLogger', () => {
   let logger: JsonLogger;
+  
+  let spyLog: jest.SpyInstance;
+  let spyWarn: jest.SpyInstance;
+  let spyError: jest.SpyInstance;
 
   beforeEach(async () => {
+    spyLog = jest.spyOn(console, 'log');
+    spyWarn = jest.spyOn(console, 'warn');
+    spyError = jest.spyOn(console, 'error');
+    
     logger = new JsonLogger();
+
+    // переопределить вывод в консоль, чтобы не спамить лишнего во время тестов
+    spyLog.mockImplementation(() => 'fn() log: mocked implementation');
+    spyWarn.mockImplementation(() => 'fn() warn: mocked implementation');
+    spyError.mockImplementation(() => 'fn() error: mocked implementation');
   });
 
-  it('.get() should call UsersService.get', () => {
+  afterEach(async () => {
+    spyLog.mockRestore();
+    spyWarn.mockRestore();
+    spyError.mockRestore();
+  });
+
+  const haveBeenCalledTimes = (logTimes: number, warnTimes: number, errorTimes: number) => {
+    expect(console.log).toHaveBeenCalledTimes(logTimes);
+    expect(console.warn).toHaveBeenCalledTimes(warnTimes);
+    expect(console.error).toHaveBeenCalledTimes(errorTimes);
+  }
+
+  it('should call console.log', () => {
+    const message = "someLog";
+
+    logger.log(message);
+
+    haveBeenCalledTimes(1, 0, 0);
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining(message));
+  });
+
+  it('should call console.warn', () => {
+    const message = "someWarning";
+    
+    logger.warn(message);
+
+    haveBeenCalledTimes(0, 1, 0);
+    expect(console.warn).toHaveBeenCalledWith(expect.stringContaining(message));
+  });
+
+  it('should call console.error', () => {
+    const message = "someError";
+    
+    logger.error(message);
+
+    haveBeenCalledTimes(0, 0, 1);
+    expect(console.error).toHaveBeenCalledWith(expect.stringContaining(message));
+  });
+  
+  it('should create a correctly formated message (without optionalParams)', () => {
+    const message = 'message';
+    const expectMessage = {
+      level: 'log',
+      message: 'message',
+      optionalParams: [],
+    };
+
+    const formatedMessage = logger.formatMessage('log', message);
+    const parsedMessage = JSON.parse(formatedMessage);
+
+    expect(parsedMessage).toEqual(expectMessage);
+  });
+  
+  it('should create a correctly formated message (with optionalParams)', () => {
+    const message = 'message';
+    const expectMessage = {
+      level: 'log',
+      message: 'message',
+      optionalParams: [
+        { key1: 'value1' },
+        { key2: 'value2' },
+      ],
+    };
+
+    const formatedMessage = logger.formatMessage('log', message,
+      { key1: 'value1' }, { key2: 'value2' });
+    const parsedMessage = JSON.parse(formatedMessage);
+
+    expect(parsedMessage).toEqual(expectMessage);
   });
 });
--- backend/src/logger/tskv.logger.spec.ts
+++ backend/src/logger/tskv.logger.spec.ts
@@ -1,13 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TskvLogger } from './tskv.logger';
 
-describe('UsersController', () => {
+describe('TskvLogger', () => {
   let logger: TskvLogger;
 
-  beforeEach(async () => {
-    logger = new TskvLogger();
+  let spyLog: jest.SpyInstance;
+  let spyWarn: jest.SpyInstance;
+  let spyError: jest.SpyInstance;
+
+  // логгер использует Date().toISOString(), можно замокировать его, чтобы не мешало проверке формата лога
+  let spyDate: jest.SpyInstance;
+
+  beforeAll(async () => {
+    spyDate = jest.spyOn(Date.prototype, 'toISOString').mockReturnValue('timestamp');
   });
 
-  it('.get() should call UsersService.get', () => {
+  afterAll(async () => {
+    spyDate.mockRestore();
+  });
+
+  beforeEach(async () => {
+    spyLog = jest.spyOn(console, 'log');
+    spyWarn = jest.spyOn(console, 'warn');
+    spyError = jest.spyOn(console, 'error');
+    
+    logger = new TskvLogger();
+
+    // переопределить вывод в консоль, чтобы не спамить лишнего во время тестов
+    spyLog.mockImplementation(() => 'fn() log: mocked implementation');
+    spyWarn.mockImplementation(() => 'fn() warn: mocked implementation');
+    spyError.mockImplementation(() => 'fn() error: mocked implementation');
+  });
+
+  afterEach(async () => {
+    spyLog.mockRestore();
+    spyWarn.mockRestore();
+    spyError.mockRestore();
+  });
+
+  const haveBeenCalledTimes = (logTimes: number, warnTimes: number, errorTimes: number) => {
+    expect(console.log).toHaveBeenCalledTimes(logTimes);
+    expect(console.warn).toHaveBeenCalledTimes(warnTimes);
+    expect(console.error).toHaveBeenCalledTimes(errorTimes);
+  };
+
+  it('should call console.log with a formated message', () => {
+    const message = 'someLog';
+
+    logger.log(message);
+
+    haveBeenCalledTimes(1, 0, 0);
+    expect(console.log).toHaveBeenCalledWith(logger.formatMessage('log', message));
+  });
+
+  it('should call console.warn with a formated message', () => {
+    const message = 'someWarning';
+    
+    logger.warn(message);
+
+    haveBeenCalledTimes(0, 1, 0);
+    expect(console.warn).toHaveBeenCalledWith(logger.formatMessage('warn', message));
+  });
+
+  it('should call console.error with a formated message', () => {
+    const message = 'someError';
+    
+    logger.error(message);
+
+    haveBeenCalledTimes(0, 0, 1);
+    expect(console.error).toHaveBeenCalledWith(logger.formatMessage('error', message));
+  });
+  
+  it('should create a correctly formated message (without optionalParams)', () => {
+    const message = 'message';
+    const expectMessage = 'level=log\ttimestamp=timestamp\tmessage=message';
+
+    const formatedMessage = logger.formatMessage('log', message);
+    expect(formatedMessage).toEqual(expectMessage);
+  });
+
+  it('should create a correctly formated message (with optionalParams)', () => {
+    const message = 'message';
+    const expectMessage = 'level=log\ttimestamp=timestamp\tmessage=message\tparam_key1=value1\tparam_key2=value2';
+
+    const formatedMessage = logger.formatMessage('log', message,
+      { key1: 'value1' }, { key2: 'value2' });
+    expect(formatedMessage).toEqual(expectMessage);
   });
 });
